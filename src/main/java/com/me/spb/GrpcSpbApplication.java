7c8b443d9185c41a48a3283095144825bc971b9f
gerry <gerry>
2023-06-16 15:10:32 -0700

    Initial commit

---
+++ src/main/java/com/me/spb/GrpcSpbApplication.java
@@ -0,0 +1,13 @@
+package com.me.spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/spb/GrpcSpbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.spb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
